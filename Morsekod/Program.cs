e9ed4917e0b84195ea3c0e774c54c6729c47b2b7
Morzun <uddha@live.se>
2021-10-21 21:08:31 +0200

    Testtest

---
+++ Morsekod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Morsekod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
